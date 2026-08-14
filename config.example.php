64d584aae333b319e6e989fe0e17ad75626a8901
Xing Haoyu <2709346590@qq.com>
2026-08-14 17:11:52 +0800

    chore: add PHP configuration template

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+// 复制为 config.php 后修改。
+// 不要把 config.php 上传到公开仓库。
+return [
+    'admin_user' => 'admin',
+    // 使用 password_hash() 生成密码哈希后填入这里。
+    'admin_password_hash' => '$2y$10$REPLACE_WITH_YOUR_PASSWORD_HASH',
+    'site_name' => 'TACTICAL BLOG'
+];
